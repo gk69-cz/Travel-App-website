af74ec7d6734cc83f4d81107a6b7675fab76a6d8
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2022-06-26 08:19:10 +0530

    initial login/signup page setup

---
+++ auth/scripts/script.js
@@ -0,0 +1,21 @@
+function Signuppage() {
+  console.log("hello");
+  document.getElementById("login").style.display = "none";
+  document.getElementById("signup").style.display = "block";
+}
+
+function Loginpage() {
+  console.log("hello");
+  document.getElementById("login").style.display = "block";
+  document.getElementById("signup").style.display = "none";
+}
+function forgotpass(){
+  alert("username = admin password =admin")
+}
+
+function Login(){
+
+}
+function Signup(){
+
+}
