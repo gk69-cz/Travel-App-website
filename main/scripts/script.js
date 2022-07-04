b37443d13959458a44966e8c67a4540ae43fddf2
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2022-07-04 21:52:08 +0530

    front face setup and navigation bar

---
+++ main/scripts/script.js
@@ -0,0 +1,21 @@
+function searching() {
+   document.getElementById("searchButton").style.display = "none";
+    document.getElementById("searchbox").style.display = "block";
+    document.getElementById("search").style.display = "block";
+}
+
+function Loginpage() {
+  console.log("hello");
+  document.getElementById("login").style.display = "block";
+  document.getElementById("signup").style.display = "none";
+}
+function forgotpass(){
+  alert("username = admin password =admin")
+}
+
+function Login(){
+
+}
+function Signup(){
+
+}
