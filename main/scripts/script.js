8312fb5389d01e23f1ed56eb4d3e9860cd90ccee
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2022-07-07 17:29:47 +0530

    details part behind package card

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -1,21 +1,10 @@
-function searching() {
-   document.getElementById("searchButton").style.display = "none";
-    document.getElementById("searchbox").style.display = "block";
-    document.getElementById("search").style.display = "block";
-}
-
-function Loginpage() {
-  console.log("hello");
-  document.getElementById("login").style.display = "block";
-  document.getElementById("signup").style.display = "none";
-}
-function forgotpass(){
-  alert("username = admin password =admin")
-}
-
-function Login(){
-
-}
-function Signup(){
-
+function infopackage(){
+  if(document.getElementById("packageinfo").style.display = "none"){
+    document.getElementById("packageinfo").style.display = "block";
+    document.getElementById("packageface").style.display = "none";
+  }
+  else{
+    document.getElementById("packageinfo").style.display = "none";
+    document.getElementById("packageface").style.display = "block";
+  }
 }
