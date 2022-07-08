75535181f59e63b85b06669dc86cf66ac5ca76ec
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2022-07-08 16:42:56 +0530

    journel and details

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -1,10 +1,13 @@
+
+function closedf(){
+  document.getElementById("packageinfo").style.display = "block";
+  document.getElementById("packageface").style.display = "none";
+}
 function infopackage(){
-  if(document.getElementById("packageinfo").style.display = "none"){
-    document.getElementById("packageinfo").style.display = "block";
-    document.getElementById("packageface").style.display = "none";
-  }
-  else{
-    document.getElementById("packageinfo").style.display = "none";
-    document.getElementById("packageface").style.display = "block";
-  }
+  document.getElementById("packageinfo").style.display = "none";
+  document.getElementById("packageface").style.display = "block";
+}
+
+function booked(){
+  alert("you will be notified shortly")
 }
