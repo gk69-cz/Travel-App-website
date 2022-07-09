6831afc3ad4704a26a45d1027aeed1638dbb9028
Christy Zachariah <69849029+gk69-cz@users.noreply.github.com>
2022-07-09 18:52:54 +0530

    packages and journels

--- main/scripts/script.js
+++ main/scripts/script.js
@@ -7,6 +7,40 @@ function infopackage(){
   document.getElementById("packageinfo").style.display = "none";
   document.getElementById("packageface").style.display = "block";
 }
+function closedf1(){
+  document.getElementById("packageinfo1").style.display = "block";
+  document.getElementById("packageface1").style.display = "none";
+}
+function infopackage1(){
+  document.getElementById("packageinfo1").style.display = "none";
+  document.getElementById("packageface1").style.display = "block";
+}
+
+function closedf2(){
+  document.getElementById("packageinfo2").style.display = "block";
+  document.getElementById("packageface2").style.display = "none";
+}
+function infopackage2(){
+  document.getElementById("packageinfo2").style.display = "none";
+  document.getElementById("packageface2").style.display = "block";
+}
+
+function left(){
+  document.getElementById("packages").style.display = "none";
+  document.getElementById("packages2").style.display = "flex";
+}
+function right(){
+  document.getElementById("packages2").style.display = "none";
+  document.getElementById("packages").style.display = "flex";
+}
+function leftj(){
+  document.getElementById("journels2").style.display = "none";
+  document.getElementById("journels").style.display = "flex";
+}
+function rightj(){
+  document.getElementById("journels").style.display = "none";
+  document.getElementById("journels2").style.display = "flex";
+}
 
 function booked(){
   alert("you will be notified shortly")
